8fecf5ccba1731c911b3f95a554c76915f40cb55
WinstonShaw <winstonsshaw@hotmail.com>
2022-01-24 17:43:57 -0300

    entrega 3ra clase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+
+const orangeColor = "orange";
+const blueColor = "blue";
+const redColor = "red";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <NavBar myColor={blueColor} />
+
+
+        
         <p>
-          Tienda W
+          Bienvenido a Tiendas W
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      
     </div>
   );
 }
 
 export default App;
+
+// <img src={logo} className="App-logo" alt="logo" />
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import "./NavBar.css";
+
+const NavBar = (props) => {
+    return(
+        <nav className="Navigation" style={{border: `10px solid ${props.myColor}`}}>
+            <h1>Tienda W</h1>
+
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Consolas</li>
+                    <li>Juegos</li>
+                    <li>Promociones</li>
+                </ul>
+            </div>
+
+            
+
+            
+        </nav>
+    );
+
+}
+
+export default NavBar;
